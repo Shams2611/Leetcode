dec163f7122aec92fad355ebfd7b2a571a22fe21
Shams Abrar <shams.abrar2611@gmail.com>
2024-01-12 16:39:33 -0500

    Ari method for sets

--- Arrays & Hashing/Contains_duplicates.js
+++ Arrays & Hashing/Contains_duplicates.js
@@ -22,3 +22,14 @@ var containsDuplicate = function (nums) {
   }
   return false;
 };
+
+// Very efficient method suggested by Ari
+
+var containsDuplicate = function (nums) {
+  let newSet = new Set(nums);
+  if (newSet.size === nums.length) {
+    return false;
+  } else {
+    return true;
+  }
+};
