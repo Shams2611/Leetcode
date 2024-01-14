25088aee433b9ba00b5a85ba84711848b5f1e69f
Shams Abrar <shams.abrar2611@gmail.com>
2024-01-14 16:51:47 -0500

    valid Anagram my initial approach

---
+++ Arrays & Hashing/validAnagram.js
@@ -0,0 +1,11 @@
+// Question link: https://leetcode.com/problems/valid-anagram/description/
+// My initial aapproach
+
+var containsDuplicate = function (nums) {
+  let newSet = new Set(nums);
+  if (newSet.size === nums.length) {
+    return false;
+  } else {
+    return true;
+  }
+};
