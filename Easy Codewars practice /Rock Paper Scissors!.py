f1e4f5be10997e2638aecaedf67ba28e2033749a
Shams Abrar <shams.abrar2611@gmail.com>
2024-01-19 12:00:00 -0500

    clever solution

--- Easy Codewars practice /Rock Paper Scissors!.py
+++ Easy Codewars practice /Rock Paper Scissors!.py
@@ -9,3 +9,10 @@ def rps(p1, p2):
         return "Draw!"
     else:
         return outcomes.get((p1, p2), "Player 2 won!")
+
+# very clever method 
+
+def rps(p1, p2):
+    hand = {'rock':0, 'paper':1, 'scissors':2}
+    results = ['Draw!', 'Player 1 won!', 'Player 2 won!']
+    return results[hand[p1] - hand[p2]]
