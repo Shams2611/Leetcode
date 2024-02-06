7884f822b10d4b755e9b16b49dcd62a4d99d5044
Shams Abrar <shams.abrar2611@gmail.com>
2024-02-06 12:00:00 -0500

    medium 5kyu

--- Easy Codewars practice /Pete, the baker.py
+++ Easy Codewars practice /Pete, the baker.py
@@ -9,3 +9,11 @@ def cakes(recipe, available):
             return 0
         
     return min(max_cakes)
+
+# Efficient solution 
+def cakes(recipe, available):
+    ''' Take each ingredient from the recipe, see if it is in the available ones
+        and calculate how many full cakes you can make with it.
+        If an ingredient is missing, we can't bake a cake. Otherwise we have to
+        find the lowest possible amount of cakes.'''
+    return min([available[i]//recipe[i] if i in available else 0 for i in recipe])
