eb6065b43609f2312e46a3b2635e19e6986769f6
Shams Abrar <shams.abrar2611@gmail.com>
2024-02-09 12:00:00 -0500

    medium 5kyu

---
+++ Easy Codewars practice /Rot13.py
@@ -0,0 +1,11 @@
+def rot13(message):
+    new_message = ""
+    for char in message:
+        if 'a' <= char <= 'z':
+            new_message += chr(((ord(char) - ord('a') + 13) % 26) + ord('a'))
+        elif 'A' <= char <= 'Z':
+            new_message += chr(((ord(char) - ord('A') + 13) % 26) + ord('A'))
+        else:
+            new_message += char
+        
+    return new_message
