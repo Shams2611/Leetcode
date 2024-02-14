6e776940044ba16a0756a38ccd32cf9f8cb811fc
Shams Abrar <shams.abrar2611@gmail.com>
2024-02-14 12:00:00 -0500

    easy lambda 7kyu

--- Easy Codewars practice /Find the capitals.py
+++ Easy Codewars practice /Find the capitals.py
@@ -5,4 +5,8 @@ def capitals(word):
         if word[i].isupper():
             new_list.append(i)
     return new_list
-        
+
+#using lambda 
+
+def capitals(word):
+    return filter(lambda x: word[x].isupper(), range(len(word)))
