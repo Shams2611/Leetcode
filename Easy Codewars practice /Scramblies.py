7afa09c419d00f3ba600f38c7d875b55291b2be8
Shams Abrar <shams.abrar2611@gmail.com>
2024-02-21 12:00:00 -0500

    medium leetcode

--- Easy Codewars practice /Scramblies.py
+++ Easy Codewars practice /Scramblies.py
@@ -20,3 +20,14 @@ def scramble(s1, s2):
         return True
     else:
         return False
+
+
+#Creative way to do it
+
+
+def scramble(s1, s2):
+    for char in set(s2):
+        if s1.count(char) < s2.count(char):
+            return False
+        
+    return True
