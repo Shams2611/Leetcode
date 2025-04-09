125c2b380a433119a580c4ca85bf55cea6b7e920
Shams Abrar <76002385+Shams2611@users.noreply.github.com>
2025-04-09 11:53:21 -0400

    Create file

---
+++ 27-RemoveElement/version/python/27-RemoveElement_20250409_115321.py
@@ -0,0 +1,11 @@
+# Last updated: 4/9/2025, 11:53:21 AM
+class Solution(object):
+    def removeElement(self, nums, val):
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            if nums[start] == val:
+                nums[start], nums[end] = nums[end], nums[start]
+                end -= 1
+            else:
+                start += 1
+        return start 
